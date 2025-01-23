e8d127c2698fbd0bf10ce934c79130ccea03a0fa
Siddhartha Paudel <sidharthpoudel04@gmail.com>
2025-01-23 21:14:52 +0545

    first commit

--- DCS-frontend/vite.config.js
+++ DCS-frontend/vite.config.js
@@ -2,8 +2,11 @@ import react from '@vitejs/plugin-react';
 
 export default {
   plugins: [react()],
+
   base:'/',
   server: {
+    host:true,
+    port:5173,
     proxy: {
       '/api': 'http://52.66.247.124:2020/',
     },
